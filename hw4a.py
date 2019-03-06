66250ace9bb6ff96464879cc94c9dd1aa63da85b
srikii <44855671+srikii@users.noreply.github.com>
2019-03-06 02:49:36 -0500

    Update hw4a.py

--- hw4a.py
+++ hw4a.py
@@ -1,19 +1,19 @@
 """program to read the number od repositories in a given username and also the number of commits for each repository
     author: srikanth"""
-
-import urllib
+    
+import urllib.request
+import urllib.parse
+import urllib.error
 import json
 import requests
+import ssl
+
 
 def git_repocommits(n):
     git_name=n
     git_url= "https://api.github.com/users/{}/repos".format(git_name)
-    dataa = requests.get(git_url)   
-    
-    #uh = urllib.request.urlopen(git_url)
-    #data = uh.read().decode()
-
-    data= dataa.text
+    uh = urllib.request.urlopen(git_url)
+    data = uh.read().decode()
 
     try:
         js = json.loads(data)
@@ -31,7 +31,8 @@ def git_repocommits(n):
 
     for i in l1:
         repo_url="https://api.github.com/repos/{}/{}/commits".format(git_name, i)
-        
+        uh2 = urllib.request.urlopen(repo_url)
+        data2 = uh2.read().decode()
         try:
             js2 = json.loads(data2)
         except:
@@ -51,6 +52,6 @@ def main():
     g=git_repocommits(n)
     
 
+
 if __name__ == '__main__':
     main()
-
