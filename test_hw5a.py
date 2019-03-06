9fabce12cebcacff40386c5adb6844fbf3a6bceb
srikii <44855671+srikii@users.noreply.github.com>
2019-03-06 00:04:12 -0500

    Update and rename test_hw4a.py to test_hw5a.py

--- test_hw5a.py
+++ test_hw5a.py
@@ -7,13 +7,21 @@ import urllib.error
 import json
 import requests
 import unittest
+from unittest import mock
+import unittest.mock
+from unittest.mock import *
 from hw4a import git_repocommits
 
 class TestRepocommits(unittest.TestCase):
-
-    def testRepocommits_srikii(self): 
+    @patch('requests.get')
+    def testRepocommits_srikii(self, mockedReq): 
         n="richkempinski"
+        mockedReq.return_value = MockResponse('[{"name":"hellogitworld"},{"name":"helloworld"},{"name":"Mocks"},{"name":"Project1"},{"name":"threads-of-life"}]')
         self.assertEqual(git_repocommits(n),(['hellogitworld', 'helloworld', 'Mocks', 'Project1', 'threads-of-life'], [30, 6, 9, 2, 1]))
+
+class MockResponse:
+    def __init__(self, blah):
+        self.text=blah
 
 if __name__ == '__main__':
     print('Running unit tests')
